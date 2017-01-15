79c8b451102d5c9bf41485a647ea513ff267387a
Qiwen Lyu <nellylyu@dhcp-v086-171.mobile.uci.edu>
2017-01-14 16:17:43 -0800

    register form added and functional

--- src/scripts/register.js
+++ src/scripts/register.js
@@ -1,15 +1,21 @@
 
-$('.reg-form').on('submit', function(e) {
-  firebase.auth().createUserWithEmailAndPassword($('#email').val(), $('#password').val()).catch(function(error) {
+$('.registerForm').on('submit', function(e) {
+  e.preventDefault();
+  console.log($('#email'));
+
+  firebase.auth().createUserWithEmailAndPassword($('#username').val(), $('#password').val())
+  .then(function(){
+    console.log("User Created! Login now!");
+    window.location.href = '/';
+  })
+  .catch(function(error) {
     // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
+    console.log(errorCode);
+    console.log(errorMessage);
     // ...
   })
 
 
 });
-
-
-console.log('#email').val());
-console.log(password);
